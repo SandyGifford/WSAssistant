597fc7d7eda8817d10b356a3d4b1c0454f466ee7
Sanford Gifford <sanford.r.gifford@gmail.com>
2021-11-04 09:54:53 -0400

    expose web socket

--- src/WSHelperServer.ts
+++ src/WSHelperServer.ts
@@ -2,42 +2,44 @@ import NodeWebSocket from "ws";
 import { WSEventType, WSHelper } from "./WSHelper";
 
 export class WSHelperServer<M> extends WSHelper<M> {
-	private ws: NodeWebSocket | null;
+	public get ws(): NodeWebSocket | null { return this._ws; };
+	private _ws: NodeWebSocket | null;
 
 	constructor(ws: NodeWebSocket) {
 		super();
-		this.ws = ws;
+		this._ws = ws;
 	}
 
 	public send = <T extends keyof M>(type: T, data?: M[T]) => {
-		if (!this.ws) return;
-		this.ws.send(JSON.stringify({ type, data }));
+		if (!this._ws) return;
+		this._ws.send(JSON.stringify({ type, data }));
 	}
 
 	public close = (): void => {
-		if (!this.ws) return;
-		this.ws.close();
-		this.ws = null;
+		if (!this._ws) return;
+		this._ws.close();
+		this._ws = null;
 	};
 
 	public addEventListener = <T extends WSEventType>(type: T, callback: (e: WebSocketEventMap[T]) => void): void => {
-		(this.ws?.addEventListener as any)(type, callback);
+		(this._ws?.addEventListener as any)(type, callback);
 	}
 
 	public removeEventListener = <T extends WSEventType>(type: T, callback: (e: WebSocketEventMap[T]) => void): void => {
-		(this.ws?.removeEventListener as any)(type, callback);
+		(this._ws?.removeEventListener as any)(type, callback);
 	}
 }
 
 export class WSSHelperServer<M> extends WSHelper<M> {
-	private wss: NodeWebSocket.Server;
+	public get wss(): NodeWebSocket.Server { return this._wss; };
+	private _wss: NodeWebSocket.Server;
 	private clients: WSHelperServer<M>[] = [];
 
 	constructor(port: number) {
 		super();
-		this.wss = new NodeWebSocket.Server({ port });
+		this._wss = new NodeWebSocket.Server({ port });
 
-		this.wss.on("connection", ws => {
+		this._wss.on("connection", ws => {
 			const client = new WSHelperServer<M>(ws);
 			client.addEventListener("close", () => {
 				const index = this.clients.indexOf(client);
@@ -53,7 +55,7 @@ export class WSSHelperServer<M> extends WSHelper<M> {
 	}
 
 	public close = (): void => {
-		this.wss.close();
+		this._wss.close();
 	};
 
 	public addEventListener = <T extends WSEventType>(type: T, callback: (client: WSHelperServer<M>, e: WebSocketEventMap[T]) => void): void => {
